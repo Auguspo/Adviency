0d9c9bd8b6cf988f9efd44d58ff39309c4cca3a7
auguspo <augustpolet@gmail.com>
2022-12-05 19:19:08 -0300

    Clean

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './css/styles.css';
- import FormRegalos from './components/form.js';
+import "./css/styles.css"
+import FormRegalos from "./components/form.js"
 
 function App() {
   return (
-    <div className="App"><div>
-      <h1>Regalos:</h1>
+    <div className="App">
+      <div className="chBox">
+        <h1>Regalos:</h1>
 
-<FormRegalos />
-
-    </div></div>
-  );
+        <FormRegalos />
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App

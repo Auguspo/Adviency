26f29ef92e21218dfb25f9dcffc327b70b7b12b9
auguspo <augustpolet@gmail.com>
2022-12-05 19:18:31 -0300

    Day 3

---
+++ Day 3/App.js
@@ -0,0 +1,15 @@
+import './css/styles.css';
+ import FormRegalos from './components/form.js';
+
+function App() {
+  return (
+    <div className="App"><div>
+      <h1>Regalos:</h1>
+
+<FormRegalos />
+
+    </div></div>
+  );
+}
+
+export default App;
